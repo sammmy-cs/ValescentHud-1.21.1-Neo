f84da457ea6e4f2da7217ce175e0a2787c5edc61
111nity <mancusosamuelcs@proton.me>
2026-08-28 09:02:27 +0200

    Fix: switch to float type

--- src/main/java/com/trinity/valescenthud/client/api/Anchor.java
+++ src/main/java/com/trinity/valescenthud/client/api/Anchor.java
@@ -1,39 +1,39 @@
-package com.xnity.valescenthud.client.api;
+package com.trinity.valescenthud.client.api;
 
 public enum Anchor {
-    TOP_LEFT(0.0, 0.0, 0, 0),
-    TOP(0.5, 0.0, 1, 0),
-    TOP_RIGHT(1.0, 0.0, 2, 0),
-    LEFT(0.0, 0.5, 0, 1),
-    CENTER(0.5, 0.5, 1, 1),
-    RIGHT(1.0, 0.5, 2, 1),
-    BOTTOM_LEFT(0.0, 1.0, 0, 2),
-    BOTTOM(0.5, 1.0, 1, 2),
-    BOTTOM_RIGHT(1.0, 1.0, 2, 2);
+    TOP_LEFT(0.0f, 0.0f, 0, 0),
+    TOP(0.5f, 0.0f, 1, 0),
+    TOP_RIGHT(1.0f, 0.0f, 2, 0),
+    LEFT(0.0f, 0.5f, 0, 1),
+    CENTER(0.5f, 0.5f, 1, 1),
+    RIGHT(1.0f, 0.5f, 2, 1),
+    BOTTOM_LEFT(0.0f, 1.0f, 0, 2),
+    BOTTOM(0.5f, 1.0f, 1, 2),
+    BOTTOM_RIGHT(1.0f, 1.0f, 2, 2);
 
     private static final Anchor[] VALUES = values();
-    private final double anchorX;
-    private final double anchorY;
+    private final float anchorX;
+    private final float anchorY;
     private final int column;
     private final int row;
 
-    Anchor(double anchorX, double anchorY, int column, int row) {
+    Anchor(float anchorX, float anchorY, int column, int row) {
         this.anchorX = anchorX;
         this.anchorY = anchorY;
         this.column = column;
         this.row = row;
     }
 
-    public double getX() {
+    public float getX() {
         return Handler.guiWidth * anchorX;
     }
-    public double getY() {
+    public float getY() {
         return Handler.guiHeight * anchorY;
     }
-    public double getOffsetX(int width) {
+    public float getOffsetX(int width) {
         return width * anchorX;
     }
-    public double getOffsetY(int height) {
+    public float getOffsetY(int height) {
         return height * anchorY;
     }
     public int getColumn() {
